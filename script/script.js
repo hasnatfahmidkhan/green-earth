d1c8c0e27001b5754caa450334633cb534502479
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-09-06 22:28:34 +0600

    added show category feature

--- script/script.js
+++ script/script.js
@@ -25,10 +25,22 @@ const loadCategory = async () => {
   );
   const data = await res.json();
   const categories = data.categories;
-  console.log(categories);
+  showCategory(categories);
 };
 
-
+// Function to display categories
+const showCategory = (categories) => {
+  categoriesContainer.innerHTML = `<li class="text-[#1F2937] hover:bg-green-700 hover:text-white rounded-md p-2 cursor-pointer ">All tress</li>`;
+  console.log(categoriesContainer);
+  // console.log(categories);
+  categories.forEach((category) => {
+    const categoryName = category.category_name;
+    console.log(categoryName);
+    categoriesContainer.innerHTML += `
+            <li class="text-[#1F2937] hover:bg-green-700 hover:text-white rounded-md p-2 cursor-pointer">${categoryName}</li>
+    `;
+  });
+};
 
 // Call Load Category Function to load category
 loadCategory();
